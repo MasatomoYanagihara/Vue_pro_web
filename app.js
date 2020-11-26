f2b7be1c9ff2c22ac0c17d0240a811d4472d6a79
Yana <masatomo.yanagihara.jp@gmail.com>
2020-11-26 19:37:19 +0900

    v-forでデータ配列の内容を表示

--- app.js
+++ app.js
@@ -28,6 +28,43 @@ Vue.createApp({
     hasTodos() {
       return this.todos.length > 0;
     },
+    resultTodos() {
+      const selectedCategory = this.selectedCategory;
+      const hideDoneTodo = this.hideDoneTodo;
+      const order = this.order;
+      const searchWord = this.searchWord;
+
+      return (
+        this.todos
+          .filter(function (todo) {
+            return (
+              selectedCategory === "" ||
+              todo.categories.indexOf(selectedCategory) !== -1
+            );
+          })
+          // 終了したものを非表示
+          .filter(function (todo) {
+            if (hideDoneTodo) {
+              return !todo.done;
+            }
+            return true;
+          })
+          // キーワード検索
+          .filter(function (todo) {
+            return (
+              todo.title.indexOf(searchWord) !== -1 ||
+              todo.description.indexOf(searchWord) !== -1
+            );
+          })
+          // 昇順・降順並べ替え
+          .sort(function (a, b) {
+            if (order === "asc") {
+              return a.dateTime - b.dateTime;
+            }
+            return b.dateTime - a.dateTime;
+          })
+      );
+    },
   },
   watch: {
     todos: {
